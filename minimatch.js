7ef735705267f6fc7a85438e80134d9596fe37a0
Eugenio Romano <eugenio.romano@alfresco.com>
2018-08-06 15:09:56 +0100

    remove path dependency

--- minimatch.js
+++ minimatch.js
@@ -2,9 +2,6 @@ module.exports = minimatch
 minimatch.Minimatch = Minimatch
 
 var path = { sep: '/' }
-try {
-  path = require('path')
-} catch (er) {}
 
 var GLOBSTAR = minimatch.GLOBSTAR = Minimatch.GLOBSTAR = {}
 var expand = require('brace-expansion')
